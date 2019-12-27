7de8a3d374ae60ca7fcc64ad68902a6dfa350e55
Adam Weissman <adam@comedytragedyepic.com>
2019-12-27 19:07:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,16 +16,28 @@ class ApplicationController < Sinatra::Base
 		erb :signup
 	end
 
-	post "/signup" do
-		#your code here!
+  post '/signup' do
+    user = User.new(username: params[:username], password: params[:password])
+		if user.save
+			redirect '/success'
+		else
+			redirect '/failure'
+		end
 	end
 
 	get "/login" do
 		erb :login
 	end
 
-	post "/login" do
-		#your code here!
+  post '/login' do
+		user = User.find_by(:username => params[:username])
+
+	  if user && user.authenticate(params[:password])
+	    session[:user_id] = user.id
+	    redirect "/success"
+	  else
+	    redirect "/failure"
+	  end
 	end
 
 	get "/success" do
